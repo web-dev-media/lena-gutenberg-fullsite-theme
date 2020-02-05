fbef06c2c04a27d6eade04784475945105307a14
Rene Reimann <rene@die-pixler.de>
2020-02-05 07:48:25 +0100

    add lazyloading basic scss structure stuff

--- assets/js/webpack.imports.js
+++ assets/js/webpack.imports.js
@@ -1,8 +1,13 @@
-document.addEventListener( 'DOMContentLoaded', function( event ) {
-	import(/* webpackChunkName: "base-styles" */'../scss/core/base.scss');
+Promise.all([
+	import(/* webpackChunkName: "base-styles" */'../scss/base.scss')
+]).then(() => {
+	window.observeElements( document.querySelectorAll( "h1,h2,h3,h4,h5,h6" ), (node) => {
+		import(/* webpackChunkName: "base-styles-headlines" */'../scss/components/headlines.scss');
+	}, {loop: false} );
 
-	// observe default headlines
-	window.observeElements( document.querySelectorAll( "h1,h2,h3,h4,h5,h6" ), () => {
-		import(/* webpackChunkName: "base-styles-headlines" */'../scss/core/headlines.scss');
+	window.observeElements( document.querySelectorAll( "blockquote" ), (node) => {
+		import(/* webpackChunkName: "base-styles-headlines" */'../scss/components/blockquote.scss');
 	}, {loop: false} );
 });
+
+
